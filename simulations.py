29450b24c4c81fc4d0c7275198ad351217044a0b
DerrickUCL <zcabxdg@ucl.ac.uk>
2020-02-09 17:19:37 +0000

    correct graph

--- simulations.py
+++ simulations.py
@@ -10,31 +10,31 @@ class Bank(object):
         self.bank_connected = [] #List of banks it owes money to
         self.bank_lent = 0 #Number of banks owing money from it
         self.edges = [] # Elements of edges: (the bank itself, the bank it borrows money from, the amount of liabilities)
-        
+
     # For NetworkX to label the bank based on ID.
     def __str__(self):
         return str(self.bank_id)
-    
+
     # This function is called when the bank connected to 'self' defaults, transmitting its loss to the asset.
     def update_total_assets(self, loss):
-        self.interbank_assets -= loss 
+        self.interbank_assets -= loss
         global EXTERNAL_ASSETS
         if(EXTERNAL_ASSETS + self.interbank_assets < self.liabilities):
             global BANK_DEFAULTS
             BANK_DEFAULTS.append(self)
-        
-    # This function is called every time 'self' borrows money from a new bank.  
+
+    # This function is called every time 'self' borrows money from a new bank.
     def update_connection(self, bank):
         self.bank_connected.append(bank)
         bank.bank_lent += 1
-    
+
     # This function is called when the bank is set to default.
     def default_bank(self):
         self.default = True
         for bank in self.bank_connected:
             if (bank.default==False):
                 bank.update_total_assets(self.interbank_assets/bank.bank_lent)
-    
+
     def total_assets(self):
         return self.interbank_assets + EXTERNAL_ASSETS
 
@@ -46,7 +46,7 @@ class Graph(object):
         self.iteration = iteration
         self.global_cascades = 0
         self.borrow_probability = 0
-    
+
     # Generate random banks and random connections between banks.
     def generate_bank(self):
         self.borrow_probability = self.average_degree/self.bank_number
@@ -54,19 +54,19 @@ class Graph(object):
             newBank = Bank(bank_id=i)
             self.bank_list.append(newBank)
         potential_lender = self.bank_list.copy()
-        
+
         for bank in self.bank_list:
             for lender in potential_lender:
                 import random
                 tendency = random.uniform(0, 1) # Generate random number. The lower, the higher chance of a connection.
                 if(bank!=lender and tendency < self.borrow_probability):
                     bank.update_connection(lender)
-        
+
         self.random_default_bank()
         self.print_banks_status()
         if(len(BANK_DEFAULTS)/self.bank_number >= 0.05):
             self.global_cascades += 1
-        
+
     def draw_graph(self):
         for i in range(0,self.iteration):
             global BANK_DEFAULTS
@@ -74,11 +74,11 @@ class Graph(object):
             self.bank_list.clear()
             self.generate_bank()
         self.store_data()
-        
+
     def print_banks_status(self):
         global BANK_DEFAULTS
         #print("Total Number of Defaults ", len(BANK_DEFAULTS))
-            
+
     def random_default_bank(self):
         import random
         random_bank = random.choice(self.bank_list)
@@ -87,7 +87,7 @@ class Graph(object):
         i = 0
         for i in range(0,len(BANK_DEFAULTS)):
             BANK_DEFAULTS[i].default_bank()
-        
+
     def store_data(self):
         import json
         average_degree = self.average_degree
@@ -101,7 +101,7 @@ class Graph(object):
 
 import numpy
 for i in numpy.arange(0,10,0.5):
-    graph = Graph(bank_number=1000, average_degree=i, iteration=100)
+    graph = Graph(bank_number=500, average_degree=i, iteration=100)
     graph.draw_graph()
 
 import matplotlib.pyplot as plt
